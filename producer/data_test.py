a180585271875b274d692148b35720c708ee9be0
Shubham Shah <shubham1996.shah@gmail.com>
2025-05-22 05:14:39 -0400

    file format changes

--- producer/data_test.py
+++ producer/data_test.py
@@ -2,17 +2,13 @@ from nba_api.live.nba.endpoints import scoreboard
 from nba_api.live.nba.endpoints import boxscore
 from nba_api.stats.endpoints import playerdashboardbyyearoveryear
 import json
+import ndjson
+import pandas as pd
 
 games = scoreboard.ScoreBoard()
 box_score = boxscore.BoxScore('0042400301')
 
 score = games.get_dict()
-
-with open("nba_score.json", "w") as f:
-    json.dump(score, f)
-
-f.close()
-
 
 
 box_score_dict = box_score.get_dict()
@@ -20,18 +16,28 @@ box_score_dict = box_score.get_dict()
 
 career = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear('1628384')
 # print(career.get_data_frames()[0]['REB'] / career.get_data_frames()[0]['GP'] )
+
+relevant_data = []
+
 games = score['scoreboard']['games']
 for game in games:
-    player_id_home = game['gameLeaders']['homeLeaders']['personId']
-    player_id_away = game['gameLeaders']['awayLeaders']['personId']
-    career_home = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(f'{player_id_home}')
-    career_away = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(f'{player_id_away}')
-    with open("home_player_stats.json", "w") as f1:
-        json.dump(career_home.get_dict(), f1)
-    f1.close()
-    with open("away_player_stats.json", "w") as f2:
-        json.dump(career_away.get_dict(), f2)
-    f2.close()
+
+    player_home_stats = game['gameLeaders']['homeLeaders']
+    player_away_stats = game['gameLeaders']['awayLeaders']
+    with open("nba_score.ndjson", "w") as f:
+        ndjson.dump([player_home_stats, player_away_stats], f)
+    player_id_home = player_home_stats['personId']
+    player_id_away = player_away_stats['personId']
+    career_home = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(f'{player_id_home}').get_data_frames()[0]
+    career_home['PERSON_ID'] = player_id_home
+    career_away = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(f'{player_id_away}').get_data_frames()[0]
+    career_away['PERSON_ID'] = player_id_away
+
+    all_player_stats = pd.concat([career_home, career_away])
+
+    all_player_stats.to_json("player_stats.ndjson",
+            orient="records",
+            lines=True)
 
 # print(score['scoreboard']['games'][0]['gameLeaders']['homeLeaders'])
 # print(len(score['scoreboard']['games']))
